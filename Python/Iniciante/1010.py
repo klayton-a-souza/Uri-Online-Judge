aadcb7d420adcaa208c17b69aa8a731208392e73
Klayton <klayton.al.souza@gmail.com>
2021-01-21 22:01:59 -0300

    Alteração no problema 1010
    Passando mais de uma variavel por linha utilizando o slipt para separar os valores.

--- Python/Iniciante/1010.py
+++ Python/Iniciante/1010.py
@@ -1,13 +1,13 @@
-codigoPeca01 = int(input()) 
-numeroPeca01 = int(input())
-valorUnitario01 = float(input())
+c1,n1,v1 = input().split(" ")
+c2,n2,v2 = input().split(" ")
 
-codigoPeca02 = int(input())
-numeroPeca02 = int(input())
-valorUnitario02 = float(input())
+c1 = int(c1)
+n1 = int(n1)
+v1 = float(v1)
 
-valorPagar01 = numeroPeca01*valorUnitario01 
-valorPagar02 = numeroPeca02*valorUnitario02
-valorPagar = valorPagar01 + valorPagar02
+c2 = int(c2)
+n2 = int(n2)
+v2 = float(v2)
 
+valorPagar = (n1*v1)+(n2*v2)
 print("VALOR A PAGAR: R$ %.2f"%valorPagar)

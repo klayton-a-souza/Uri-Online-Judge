1ca57730e6d96e7d65bb5bc375e7ab669fb55d6b
Klayton <klayton.al.souza@gmail.com>
2021-01-22 10:40:04 -0300

    Distância Entre Dois Pontos
    Organização da formula

--- Python/Iniciante/1015.py
+++ Python/Iniciante/1015.py
@@ -6,6 +6,8 @@ y1 = float(y1)
 x2 = float(x2)
 y2 = float(y2)
 
-distancia = (((x2-x1)**2)+((y2 -y1)**2)**0.5)
+calculoX = (x2-x1)**2
+calculoY = (y2-y1)**2
+distancia = (calculoX+calculoY)**0.5
 
 print("%.4f"%distancia)

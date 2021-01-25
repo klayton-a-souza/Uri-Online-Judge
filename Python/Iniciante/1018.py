8f767604ab4a50ac83c67d0f4b6121155efc5ced
Klayton <klayton.al.souza@gmail.com>
2021-01-25 17:47:31 -0300

    Cédulas
    Usando for

--- Python/Iniciante/1018.py
+++ Python/Iniciante/1018.py
@@ -1,19 +1,7 @@
-#Solução Ruim mudar, olhar como funciona o .format e o round.
 valor = int(input())
+notas = [100,50,20,10,5,2,1]
 
-numeroCedulas = 0
-
-print(valor)
-print(valor//100 , "nota(s) de R$ 100,00")
-valor = valor%100
-print(valor//50, "nota(s) de R$ 50,00")
-valor = valor%50
-print(valor//20, "nota(s) de R$ 20,00")
-valor = valor%20
-print(valor//10, "nota(s) de R$ 10,00")
-valor = valor%10
-print(valor//5, "nota(s) de R$ 5,00")
-valor = valor%5
-print(valor//2, "nota(s) de R$ 2,00")
-valor =  valor%2
-print(valor//1, "nota(s) de R$ 1,00")
+for nota in notas:
+    quantNotas = int(valor/nota)
+    print("{} nota(s) de R$ {:.2f}".format(quantNotas,nota))
+    valor -= quantNotas * nota

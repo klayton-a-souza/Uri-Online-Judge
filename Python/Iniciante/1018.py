2a8969415017618727f934ac3dbb49bad652a089
Klayton <klayton.al.souza@gmail.com>
2021-01-25 17:57:58 -0300

    Cédulas
    Modificação do {}

--- Python/Iniciante/1018.py
+++ Python/Iniciante/1018.py
@@ -1,7 +1,7 @@
 valor = int(input())
-notas = [100,50,20,10,5,2,1]
-
+notas = [100, 50, 20, 10, 5, 2 , 1]
+print(valor)
 for nota in notas:
-    quantNotas = int(valor/nota)
-    print("{} nota(s) de R$ {:.2f}".format(quantNotas,nota))
+    quantNotas = int((valor/nota))
+    print("{} nota(s) de R$ {},00".format(quantNotas,nota))
     valor -= quantNotas * nota

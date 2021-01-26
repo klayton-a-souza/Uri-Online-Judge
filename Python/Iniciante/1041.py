bf07716ca69937e1fe571d4136c5598f61365233
Klayton <klayton.al.souza@gmail.com>
2021-01-26 09:43:09 -0300

    Coordenadas de um Ponto

---
+++ Python/Iniciante/1041.py
@@ -0,0 +1,19 @@
+X,Y = input().split(" ")
+
+X = float(X)
+Y = float(Y)
+
+if ((X > 0 ) and (Y > 0)):
+    print("Q1")
+elif((X < 0) and (Y > 0)):
+    print("Q2")
+elif((X < 0 ) and (Y < 0)):
+    print("Q3")
+elif((X > 0) and (Y < 0)):
+    print("Q4")
+elif ((X == 0) and (Y == 0)):
+    print("Origem")
+elif(X == 0):
+    print("Eixo Y")
+elif(Y == 0):
+    print("Eixo X")
